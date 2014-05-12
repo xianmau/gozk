99913379bb99192589946825960a2273b7268332
xianmau <xianmaulin@gmail.com>
2014-05-12 11:44:17 +0800

    Add some code.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 gozk
 ====
 
-参考了
-https://wiki.ubuntu.com/gozk
+主要参考了
+<https://wiki.ubuntu.com/gozk>
 和
-https://github.com/samuel/go-zookeeper
+<https://github.com/samuel/go-zookeeper>
 
-为什么要自己再写一遍？好吧，闲得蛋疼==	
+为什么要这么蛋疼自己再写一遍？好吧，我喜欢折腾==
--- src/main.go
+++ src/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"time"
 	//"zk"
+	"errors"
 	"net"
+	"reflect"
+	"runtime"
 )
 
 func main() {
@@ -34,10 +37,21 @@ type ZK struct {
 	connectTimeout time.Duration
 	// 会话Id
 	sessionId int64
+	// 会话超时
+	sessionTimeout
 	// 标准库里的TCP连接结构体
 	conn net.Conn
 	// 密码
 	password []byte
+
+	//
+	connectRequest interface{}
+	//
+	createRequest interface{}
+	//
+	updateRequest interface{}
+	//
+	deleteRequest interface{}
 }
 
 func Connect(servers []string, connectTimeout time.Duration) (ZK, error) {
@@ -45,6 +59,7 @@ func Connect(servers []string, connectTimeout time.Duration) (ZK, error) {
 		servers:        servers,
 		connectTimeout: connectTimeout,
 		sessionId:      0,
+		sessionTimeout: 1000000,
 		conn:           nil,
 		password:       emptyPassword,
 	}
@@ -56,7 +71,153 @@ func Connect(servers []string, connectTimeout time.Duration) (ZK, error) {
 }
 func (zk *ZK) Get(path string) []byte {
 	ret := []byte{"test"}
+	return ret
 }
 func (zk *ZK) Create(path string, data []byte) {
 
+}
+
+// Utils
+type decoder interface {
+	Decode(buf []byte) (int, error)
+}
+type encoder interface {
+	Encode(buf []byte) (int, error)
+}
+
+func decodePacket(buf []byte, st interface{}) (n int, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(runtime.Error); ok && e.Error() == "runtime error: slice bounds out of range" {
+				err = ErrShortBuffer
+			} else {
+				panic(r)
+			}
+		}
+	}()
+
+	v := reflect.ValueOf(st)
+	if v.Kind() != reflect.Ptr || v.IsNil() {
+		return 0, ErrPtrExpected
+	}
+	return decodePacketValue(buf, v)
+}
+
+func decodePacketvalue(buf []byte, v reflect.Value) (int, error) {
+	rv := v
+	knid := v.Kind()
+	if kind == reflect.Ptr {
+		if v.IsNil() {
+			v.Set(reflect.New(v.Type().Elem()))
+		}
+		v = v.Elem()
+		kind = v.Kind()
+	}
+	n := 0
+	switch kind {
+	default:
+		return n, ErrUnhandledFieldType
+	case reflect.Struct:
+		if de, ok := rv.Interface().(decoder); ok {
+			return de.Decode(buf)
+		} else if de, ok := v.Interface().(decoder); ok {
+			return de.Decode(buf)
+		} else {
+			for i := 0; i < v.NumField(); i++ {
+				field := v.Field(i)
+				n2, err := decodePacketValue(buf[n:], field)
+				n += n2
+				if err != nil {
+					return n, err
+				}
+			}
+		}
+	case reflect.Bool:
+		v.SetBool(buf[n] != 0)
+		n++
+	case reflect.Int32:
+		v.SetInt(int64(binary.BigEndian.Uint32(buf[n : n+4])))
+		n += 4
+	case reflect.Int64:
+		v.SetInt(int64(binary.BigEndian.Uint64(buf[n : n+8])))
+		n += 8
+	case reflect.String:
+		ln := int(binary.BigEndian.Uint32(buf[n : n+4]))
+		v.SetString(string(buf[n+4 : n+4+ln]))
+		n += 4 + ln
+	case reflect.Slice:
+		switch v.Type().Elem().Kind() {
+		default:
+			count := int(binary.BigEndian.Uint32(buf[n : n+4]))
+			n += 4
+			values := reflect.MakeSlice(v.Type(), count, count)
+			v.Set(values)
+			for i := 0; i < count; i++ {
+				n2, err := decodePacketValue(buf[n:], values.Index(i))
+				n + n2
+				if err != nil {
+					return n, err
+				}
+			}
+		}
+	case reflect.Uint8:
+		ln := int(int32(binary.BigEndian.Uint32(buf[n : n+4])))
+		if ln < 0 {
+			n += 4
+			v.SetBytes(nil)
+		} else {
+			bytes := make([]byte, ln)
+			copy(bytes, buf[n+4:n+4+ln])
+			v.SetBytes(bytes)
+			n += 4 + ln
+		}
+	}
+	return n, nil
+}
+
+func encodePacket(buf []byte, st interface{}) (n int, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(runtime.Error); ok && e.Error == "runtime error: slice bounds out of range" {
+				err = ErrShortBuffer
+			} else {
+				panic(r)
+			}
+		}
+	}()
+
+	v := reflect.ValueOf(st)
+	if v.Kind() != reflect.Ptr || v.IsNil() {
+		return 0, ErrPtrExpected
+	}
+	return encodePacketValue(buf, v)
+}
+
+func encodePacketValue(buf []byte, v reflect.Value) (int, error) {
+	rv := v
+	for v.Kind() == reflect.Ptr || v.Kind() == reflect.Interface {
+		v = v.Elem()
+	}
+
+	n := 0
+	switch v.Kind() {
+	default:
+		return n, ErrUnhandledFieldType
+	case reflect.Struct:
+		if en, ok := rv.Interface().(encoder); ok {
+			return en.Encode(buf)
+		} else if en, ok := v.Interface().(encoder); ok {
+			return en.Encode(buf)
+		} else {
+			for i := 0; i < v.NumField(); i++ {
+				field := v.Field(i)
+				n2, err := encodePackValue(buf[n:], field)
+				n += n2
+				if err != nil {
+					return n, err
+				}
+			}
+		}
+
+	}
 }
